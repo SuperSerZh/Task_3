9f335cc9d55cf3fc7e979ab43007d3611cee7b4d
SuperSerZh <serzhik05@mail.ru>
2021-05-05 23:50:11 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter you age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You name is {0} and age is {1}", name, age);
+
+            Console.Write("Enter you birthdate: ");
+            var birth_date = Console.ReadLine();
+            Console.WriteLine("You birthdate is {0} ", birth_date);
+            Console.ReadKey();
+        }
+    }
+}
